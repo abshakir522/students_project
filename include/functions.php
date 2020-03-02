625a86c6dfbc5435d4b6ff425a394516ece8fa2c
Abdullah Shakir <abdullahshakir82@gmail.com>
2020-03-02 16:32:17 +0500

    content region utuilized

--- include/functions.php
+++ include/functions.php
@@ -32,6 +32,79 @@
     return $page_set;
 
   }
+  
+
+  function get_subject_by_id($subject_id){
+
+    global $connection;
+    $query  = "SELECT * ";
+    $query .= "FROM subjects ";
+    $query .= "WHERE id={$subject_id} ";
+    $query .= "LIMIT 1";
+    $subject_set = mysqli_query($connection, $query);
+    test_query($subject_set);
+    $subject = mysqli_fetch_assoc($subject_set);
+    return $subject;
+
+  }
+
+
+  function get_page_by_id($page_id){
+
+    global $connection;
+    $query  = "SELECT * ";
+    $query .= "FROM pages ";
+    $query .= "WHERE id={$page_id} ";
+    $query .= "LIMIT 1";
+    $pages_set = mysqli_query($connection, $query);
+    test_query($pages_set);
+    $page = mysqli_fetch_assoc($pages_set);
+    return $page;
+
+  }
+
+
+  function navigation($subject_array, $page_array){
+    $output = "<ul class=\"subjects\">";
+       global $subject_set;
+       global $page_set;
+				while($subject = mysqli_fetch_assoc($subject_set)) {
+          $output .="<li";
+          $output .="class=\"";
+          if($subject['id'] == $subject_array['id']){ 
+            $output .="selected";
+          }
+          $output .="\">";
+
+          $output .="<a href=\"manage_content.php?subject={$subject['id']}\">";
+          $output .= $subject["menu_name"];
+          $output .="</a>";
+          $page_set = get_pages_for_subject($subject["id"]);
+          $output .= "<ul class=\"pages\">";
+          while($page = mysqli_fetch_assoc($page_set)) {
+            $output .= "<li class=\"";
+            if($page['id'] == $page_array['id']){ 
+              $output .= "selected";
+            }
+            $output .= "\">";
+            $output .= "<a href=\"manage_content.php?page={$page['id']}\">";
+            $output .= $page["menu_name"]; 
+            $output .= "</a>";
+            $output .="</li>";
+          }
+            
+              $output .= "</ul>";
+              $output .="</li>";
+			} 
+		    
+      $output .="</ul>";
+    mysqli_free_result($subject_set);
+    return $output;
+
+  }
+  
+
+  
 
 
 
--- public/manage_content.php
+++ public/manage_content.php
@@ -2,16 +2,17 @@
 <?php require_once("../include/functions.php"); ?>
 <?php 
 	if(isset($_GET['subject'])){
-		$selected_subject = $_GET['subject'];
-		$selected_page = null;
+		$current_subject = get_subject_by_id($_GET['subject']);
+		$current_page = null;
 
 	}elseif(isset($_GET['page'])){
-		$selected_page = $_GET['page'];
-		$selected_subject = null;
+		$current_page = get_page_by_id($_GET['page']);
+		$current_subject = null;
 
 	}else{
-		$selected_subject = null;
-		$selected_page = null;
+		$current_subject = null;
+		$current_page = null;
+		
 	}
 
 ?>
@@ -22,42 +23,25 @@
 <?php include("../include/layout/header.php") ?>
 <div id="main">
     <div id="navigation">
-		<ul class="subjects">
-			<?php
-				// 3. Use returned data (if any)
-				while($subject = mysqli_fetch_assoc($subject_set)) {
-					// output data from each row
-			?>
-
-				<li class="<?php if($subject['id'] == $selected_subject){ echo "selected";}?>">
-					<a href="manage_content.php?subject=<?= $subject['id']?>"><?php echo $subject["menu_name"]; ?></a>
-					
-					<?php
-						// 2. Perform database query
-						$page_set = get_pages_for_subject($subject["id"]);
-					?>
-					<ul class="pages">
-					<?php
-						while($page = mysqli_fetch_assoc($page_set)) {
-					?>
-						<li class="<?php if($page['id'] == $selected_page){ echo "selected";}?>">
-							<a href="manage_content.php?page=<?= $page['id']; ?>"><?php echo $page["menu_name"]; ?></a>
-						</li>
-					<?php } ?>
-					
-					</ul>
-				</li>
-			<?php } ?>
-		    
-		</ul>
-		<?php mysqli_free_result($subject_set); ?>
-		</div>
+		<?php echo navigation($current_subject ,$current_page);?>
+	</div>
 
     <div id="page">
-		<h2>Manage Content</h2>
-		
-		<?php echo $selected_subject; ?>
-		<?php echo $selected_page; ?>
+		<?php if ($current_subject) { ?>
+				<h1>Manage Subject</h1>
+		<?php 
+			echo $current_subject['menu_name'];
+
+			}elseif($current_page){ ?>
+			<h2>Mange Page</h2>
+			<?php
+			echo $current_page['menu_name'];
+
+			}else{
+				echo "Please select a subject or page";
+				
+			}
+		?>
     </div>
 </div>
 <?php include("../include/layout/footer.php") ?>
